74ea09de2ac7ecb2767f7012a8e28a10bb14a9a1
Ayeza <ayezabashir442@gmail.com>
2024-07-18 18:52:19 +0500

    configure router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { Home } from './container'
 
 const App = () => {
     return (
-        <div className='w-screen h-screen flex justify-center items-center'>
-            hello world
+        <div className='w-screen h-screen flex justify-start items-start overflow-hidden'>
+            <Routes>
+                <Route path='/home/*' element={<Home />} />
+                {/* by default it will navigate to home page */}
+                <Route path='*' element={<Navigate to={'/home'} />} />
+            </Routes>
         </div>
     )
 }

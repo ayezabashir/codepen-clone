0af1a3c906e2da608fa1ef2a983338ffbbb5302f
Ayeza <ayezabashir442@gmail.com>
2024-07-21 22:53:40 +0500

    user authentication

--- src/container/SignUp.jsx
+++ src/container/SignUp.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { Logo } from "../assets";
+import { UserAuthInput } from "../components";
+import { FaEnvelope } from "react-icons/fa6";
+import { MdPassword } from "react-icons/md";
 
 const SignUp = () => {
-  return <div className="text-white">signup</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className="w-full py-6">
+      <img
+        src={Logo}
+        className="object-contain w-32 opacity-50 h-auto"
+        alt="logo"
+      />
+      <div className="w-full flex flex-col items-center justify-center py-8 ">
+        <p className="py-12 text-2xl text-primaryText">Join With Us! 🤩</p>
+        <div className="px-8 w-full md:w-auto py-4 rounded-xl bg-secondary shadow-md flex flex-col justify-center items-center gap-8 ">
+          {/* Email */}
+          <UserAuthInput
+            label="Email"
+            placeHolder="Email"
+            isPass={false}
+            key="Email"
+            setStateFunction={setEmail}
+            Icon={FaEnvelope}
+          />
+          {/* Password */}
+          <UserAuthInput
+            label="Password"
+            placeHolder="Password"
+            isPass={true}
+            key="Password"
+            setStateFunction={setPassword}
+            Icon={MdPassword}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;

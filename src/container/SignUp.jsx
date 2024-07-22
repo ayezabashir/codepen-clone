96c45f6dad0399e8468662a8434204383ceb5d13
Ayeza <ayezabashir442@gmail.com>
2024-07-22 22:05:15 +0500

    sign up form

--- src/container/SignUp.jsx
+++ src/container/SignUp.jsx
@@ -3,12 +3,14 @@ import { Logo } from "../assets";
 import { UserAuthInput } from "../components";
 import { FaEnvelope } from "react-icons/fa6";
 import { MdPassword } from "react-icons/md";
+import { motion } from "framer-motion";
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [getEmailValidationStatus, setIsEmailValidationStatus] =
     useState(false);
+  const [isLogin, setIsLogin] = useState(false);
   return (
     <div className="w-full py-6">
       <img
@@ -38,6 +40,43 @@ const SignUp = () => {
             setStateFunction={setPassword}
             Icon={MdPassword}
           />
+          {/* Login Button */}
+          {!isLogin ? (
+            <motion.div
+              whileTap={{ scale: 0.9 }}
+              className="flex items-center justify-center px-3 py-3 rounded-xl hover:bg-emerald-400 w-full cursor-pointer bg-emerald-500"
+            >
+              <p className="text-xl text-white">Sign Up</p>
+            </motion.div>
+          ) : (
+            <motion.div
+              whileTap={{ scale: 0.9 }}
+              className="flex items-center justify-center px-3 py-3 rounded-xl hover:bg-emerald-400 w-full cursor-pointer bg-emerald-500"
+            >
+              <p className="text-xl text-white">Login</p>
+            </motion.div>
+          )}
+          {!isLogin ? (
+            <p className="text-sm text-primaryText flex items-center justify-center gap-3">
+              Already Have an account!{" "}
+              <span
+                onClick={() => setIsLogin(!isLogin)}
+                className="text-emerald-500 cursor-pointer"
+              >
+                Login Here
+              </span>
+            </p>
+          ) : (
+            <p className="text-sm text-primaryText flex items-center justify-center gap-3">
+              Don't have account!{" "}
+              <span
+                onClick={() => setIsLogin(!isLogin)}
+                className="text-emerald-500 cursor-pointer"
+              >
+                Create Here
+              </span>
+            </p>
+          )}
         </div>
       </div>
     </div>

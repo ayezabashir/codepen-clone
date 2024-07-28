f3f8ad413900c441260636b31036d678ef9bb16f
Ayeza <ayezabashir442@gmail.com>
2024-07-28 23:25:11 +0500

    signin with github

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,10 +1,16 @@
-import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth"
+import { GithubAuthProvider, GoogleAuthProvider, signInWithRedirect } from "firebase/auth"
 import { auth } from "../config/firebase.config"
 const googleProvider = new GoogleAuthProvider()
-
+const githubProvider = new GithubAuthProvider()
 
 export const signInWithGoogle = async () => {
     await signInWithRedirect(auth, googleProvider).then(userCredentials => {
+        window.location.reload()
+    })
+}
+
+export const signInWithGithub = async () => {
+    await signInWithRedirect(auth, githubProvider).then(userCredentials => {
         window.location.reload()
     })
 }

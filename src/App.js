a392e0c11fa0ac3ba9985915c9e2a11a37c9ebea
Ayeza <ayezabashir442@gmail.com>
2024-08-02 15:28:16 +0500

    save to firestore

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Navigate, Route, Routes, useNavigate } from 'react-router-dom'
 import { Home } from './container'
-import { auth } from './config/firebase.config';
+import { auth, db } from './config/firebase.config';
+import { doc, setDoc } from 'firebase/firestore';
+import spinner from "./assets/img/loading.svg"
 
 const App = () => {
+    const [isLoading, setIsLoading] = useState(false);
     const navigate = useNavigate();
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged((userCredentials) => {
             if (userCredentials) {
                 console.log(userCredentials?.providerData[0]);
+                setDoc(doc(db, "users", userCredentials?.uid), userCredentials?.providerData[0]).then(() => {
+                    // dispatch
+                })
             } else {
                 navigate("/home/auth", { replace: true })
             }
-
         })
+        return () => unsubscribe();
     }, [])
     return (
-        <div className='w-screen h-screen flex justify-start items-start overflow-hidden'>
-            <Routes>
-                <Route path='/home/*' element={<Home />} />
-                {/* by default it will navigate to home page */}
-                <Route path='*' element={<Navigate to={'/home'} />} />
-            </Routes>
-        </div>
+        <>
+            {
+                isLoading ? <div
+                    className='h-screen bg-white w-screen flex items-center justify-center overflow-hidden'
+                >
+                    <img src={spinner} alt="loading" className='w-20 h-20 bg-transparent' />
+
+                </div> : <div className='w-screen h-screen flex justify-start items-start overflow-hidden'>
+                    <Routes>
+                        <Route path='/home/*' element={<Home />} />
+                        {/* by default it will navigate to home page */}
+                        <Route path='*' element={<Navigate to={'/home'} />} />
+                    </Routes>
+                </div>
+            }
+        </>
     )
 }
 

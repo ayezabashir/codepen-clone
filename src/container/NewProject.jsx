d39e8a21fcc42142c7b1080a22b6cf1f7b1e5092
Ayeza <ayezabashir442@gmail.com>
2024-08-11 18:39:55 +0500

    split screens

--- src/container/NewProject.jsx
+++ src/container/NewProject.jsx
@@ -1,3 +1,5 @@
+import { FaChevronDown, FaCss3, FaHtml5, FaJs } from "react-icons/fa";
+import { FcSettings } from "react-icons/fc";
 import SplitPane from "react-split-pane";
 
 const NewProject = () => {
@@ -20,10 +22,53 @@ const NewProject = () => {
             {/* top coding section */}
             <SplitPane split="vertical" minSize={500}>
               {/* html code */}
+              <div className="w-full h-full flex flex-col items-start justify-start">
+                <div className="w-full flex items-center justify-between">
+                  <div className="bg-secondary px-4 py-2 border-t-4 flex items-center justify-center gap-3 border-t-gray-500">
+                    <FaHtml5 className="text-xl text-red-500" />
+                    <p className="text-primaryText font-semibold">HTML</p>
+                  </div>
+                  {/* icons */}
+                  <div className="cursor-pointer flex items-center justify-center gap-4 px-4">
+                    <FcSettings className="text-xl" />
+                    <FaChevronDown className="text-xl text-primaryText" />
+                  </div>
+                </div>
+                <div>Code Mirror</div>
+              </div>
+
               <SplitPane split="vertical" minSize={500}>
                 {/* css code */}
+                <div className="w-full h-full flex flex-col items-start justify-start">
+                  <div className="w-full flex items-center justify-between">
+                    <div className="bg-secondary px-4 py-2 border-t-4 flex items-center justify-center gap-3 border-t-gray-500">
+                      <FaCss3 className="text-xl text-sky-500" />
+                      <p className="text-primaryText font-semibold">CSS</p>
+                    </div>
+                    {/* icons */}
+                    <div className="cursor-pointer flex items-center justify-center gap-4 px-4">
+                      <FcSettings className="text-xl" />
+                      <FaChevronDown className="text-xl text-primaryText" />
+                    </div>
+                  </div>
+                  <div>Code Mirror</div>
+                </div>
 
                 {/* js code */}
+                <div className="w-full h-full flex flex-col items-start justify-start">
+                  <div className="w-full flex items-center justify-between">
+                    <div className="bg-secondary px-4 py-2 border-t-4 flex items-center justify-center gap-3 border-t-gray-500">
+                      <FaJs className="text-xl text-yellow-500" />
+                      <p className="text-primaryText font-semibold">JS</p>
+                    </div>
+                    {/* icons */}
+                    <div className="cursor-pointer flex items-center justify-center gap-4 px-4">
+                      <FcSettings className="text-xl" />
+                      <FaChevronDown className="text-xl text-primaryText" />
+                    </div>
+                  </div>
+                  <div>Code Mirror</div>
+                </div>
               </SplitPane>
             </SplitPane>
 

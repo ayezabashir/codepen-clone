dc4b55287b0d10bc567cf2154c068bcd1e172cb5
Ayeza <ayezabashir442@gmail.com>
2024-08-15 20:54:45 +0500

    project card customization

--- src/container/Projects.jsx
+++ src/container/Projects.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import ProjectCard from "./ProjectCard";
 
 const Projects = () => {
-  return <div className="text-white">Projects</div>;
+  const projects = useSelector((state) => state.projects?.projects);
+  return (
+    <div className="w-full py-6 flex items-center justify-center gap-6 flex-wrap">
+      {projects &&
+        projects.map((project, index) => (
+          <ProjectCard key={project.id} project={project} index={index} />
+        ))}
+    </div>
+  );
 };
 
 export default Projects;

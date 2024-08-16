ca9c4e8ab694bc0c4b7e8c52eb1b2aee968d3aa6
Ayeza <ayezabashir442@gmail.com>
2024-08-16 19:40:07 +0500

    styles

--- src/container/ProjectCard.jsx
+++ src/container/ProjectCard.jsx
@@ -10,7 +10,7 @@ const ProjectCard = ({ project, index }) => {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 0.5, delay: index * 0.5 }}
-      className="w-full cursor-pointer md:w-[450px] h-[375px] bg-secondary rounded-md p-4 flex flex-col items-center justify-center gap-4 "
+      className="w-full cursor-pointer md:w-[400px] h-[375px] bg-secondary rounded-md p-4 flex flex-col items-center justify-center gap-4 "
     >
       <div
         className="bg-primary w-full h-full rounded-md overflow-hidden"
